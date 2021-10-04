acdee671ca118942465859b5fd2a8664345ba849
programadorleandrosantos <programadorleandrosantos@gmail.com>
2021-10-04 19:59:00 -0300

    chore: adding reduce

---
+++ Filters/reduce.ts
@@ -0,0 +1,7 @@
+// Array of numbers
+const arrNumber = [0,1,2,3,4,5,6,7,8,9];
+
+const sumResultFromArr = arrNumber.reduce((prev, next)=> prev + next);
+
+console.log(sumResultFromArr);
+// output : 45
